51b3e5afb1a33a89c55be6f591bbef3950cb6d2b
Berto405 <146853279+Berto405@users.noreply.github.com>
2024-03-06 16:32:06 +0800

    Fix
    -Logged in and not logged in user cant access admin home page

--- admin/home.php
+++ admin/home.php
@@ -1,6 +1,20 @@
 <?php
 include('../header.php');
 include('../dbconn.php');
+
+// Check if user is not logged in
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role'])) {
+    // Redirect users who are not logged in to the login page
+    header("Location: ../login.php");
+    exit();
+}
+
+// Check if user is not admin
+if ($_SESSION['user_role'] !== 'admin') {
+    // Redirect non-admin users to index.php
+    header("Location: ../index.php");
+    exit();
+}
 ?>
 
 <h1>Welcome,
