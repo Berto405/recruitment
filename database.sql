b63a244f461b1aab6fc7e48d2be7f5feaf499129
Berto405 <146853279+Berto405@users.noreply.github.com>
2024-03-26 10:44:32 +0800

    UPDATE DATABASE

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2024 at 09:23 AM
+-- Generation Time: Mar 26, 2024 at 03:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,6 +40,45 @@ CREATE TABLE `character_references` (
   `name_two_contact_number` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `character_references`
+--
+
+INSERT INTO `character_references` (`id`, `user_id`, `name_one`, `name_one_position`, `name_one_company`, `name_one_contact_number`, `name_two`, `name_two_position`, `name_two_company`, `name_two_contact_number`) VALUES
+(1, 2, 's', '', '', 0, '', '', '', 0),
+(2, 2, '', '', '', 0, '', '', '', 0),
+(3, 2, 's', '', '', 0, '', '', '', 0),
+(4, 2, 's', '', '', 0, '', '', '', 0),
+(5, 2, 's', '', '', 0, '', '', '', 0),
+(6, 2, 's', '', '', 0, '', '', '', 0),
+(7, 2, 's', '', '', 0, '', '', '', 0),
+(8, 2, 's', '', '', 0, '', '', '', 0),
+(9, 2, '', '', '', 0, '', '', '', 0),
+(10, 2, 's', '', '', 0, '', '', '', 0),
+(11, 2, 's', '', '', 0, '', '', '', 0),
+(12, 2, 's', '', '', 0, '', '', '', 0),
+(13, 2, 's', '', '', 0, '', '', '', 0),
+(14, 2, 's', '', '', 0, '', '', '', 0),
+(15, 2, 's', '', '', 0, '', '', '', 0),
+(16, 2, 's', '', '', 0, '', '', '', 0),
+(17, 2, 's', '', '', 0, '', '', '', 0),
+(18, 2, 's', '', '', 0, '', '', '', 0),
+(19, 2, 's', '', '', 0, '', '', '', 0),
+(20, 2, '', '', '', 0, '', '', '', 0),
+(21, 2, '', '', '', 0, '', '', '', 0),
+(22, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(23, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(24, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(25, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(26, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(27, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(28, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(29, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(30, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(31, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(32, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0),
+(33, 9, 'das', 'asd', 'asd', 42, 'asd', 'ae', 'ewq', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -50,22 +89,29 @@ CREATE TABLE `educational_attainment` (
   `id` int(11) NOT NULL,
   `user_id` int(12) NOT NULL,
   `college` varchar(255) NOT NULL,
-  `college_from` year(4) NOT NULL,
-  `college_to` year(4) NOT NULL,
+  `college_from` date NOT NULL,
+  `college_to` date NOT NULL,
   `college_degree` varchar(255) NOT NULL,
   `vocational` varchar(255) NOT NULL,
-  `vocational_from` year(4) NOT NULL,
-  `vocational_to` year(4) NOT NULL,
+  `vocational_from` date NOT NULL,
+  `vocational_to` date NOT NULL,
   `vocational_diploma` varchar(255) NOT NULL,
   `high_school` varchar(255) NOT NULL,
-  `high_school_from` year(4) NOT NULL,
-  `high_school_to` year(4) NOT NULL,
+  `high_school_from` date NOT NULL,
+  `high_school_to` date NOT NULL,
   `high_school_level` varchar(255) NOT NULL,
   `elementary` varchar(255) NOT NULL,
-  `elementary_from` year(4) NOT NULL,
-  `elementary_to` year(4) NOT NULL,
+  `elementary_from` date NOT NULL,
+  `elementary_to` date NOT NULL,
   `elementary_level` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `educational_attainment`
+--
+
+INSERT INTO `educational_attainment` (`id`, `user_id`, `college`, `college_from`, `college_to`, `college_degree`, `vocational`, `vocational_from`, `vocational_to`, `vocational_diploma`, `high_school`, `high_school_from`, `high_school_to`, `high_school_level`, `elementary`, `elementary_from`, `elementary_to`, `elementary_level`) VALUES
+(33, 9, 'dasd', '0000-00-00', '0000-00-00', 'asd', 'da', '0000-00-00', '0000-00-00', 'a', 'asd', '0000-00-00', '0000-00-00', 'adad', 'asd', '0000-00-00', '0000-00-00', 'asd');
 
 -- --------------------------------------------------------
 
@@ -104,6 +150,13 @@ CREATE TABLE `employment_background` (
   `recent_employment_position` varchar(255) NOT NULL,
   `recent_employment_contact_number` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employment_background`
+--
+
+INSERT INTO `employment_background` (`id`, `user_id`, `company_one`, `company_one_position`, `company_one_from`, `company_one_to`, `company_one_status`, `company_one_responsibilities`, `company_one_reason_for_leaving`, `company_one_last_salary`, `company_two`, `company_two_position`, `company_two_from`, `company_two_to`, `company_two_status`, `company_two_responsibilities`, `company_two_reason_for_leaving`, `company_two_last_salary`, `company_three`, `company_three_position`, `company_three_from`, `company_three_to`, `company_three_status`, `company_three_responsibilities`, `company_three_reason_for_leaving`, `company_three_last_salary`, `recent_employment_contact_person`, `recent_employment_position`, `recent_employment_contact_number`) VALUES
+(34, 9, 'das', 'asd', '0000', '0000', '', 'asd', 'asd', 0, 'asd', 'asd', '0000', '0000', '', 'asd', 'asd', 0, 'asd', 'w3eq', '0000', '0000', '', 'ewq', 'trtr', 0, 'das', 'asd', 321);
 
 -- --------------------------------------------------------
 
@@ -190,26 +243,33 @@ CREATE TABLE `lectures_and_seminars_attended` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title_one` varchar(255) NOT NULL,
-  `title_one_from` year(4) NOT NULL,
-  `title_one_to` year(4) NOT NULL,
+  `title_one_from` date NOT NULL,
+  `title_one_to` date NOT NULL,
   `title_one_venue` varchar(255) NOT NULL,
   `title_two` varchar(255) NOT NULL,
-  `title_two_from` year(4) NOT NULL,
-  `title_two_to` year(4) NOT NULL,
+  `title_two_from` date NOT NULL,
+  `title_two_to` date NOT NULL,
   `title_two_venue` varchar(255) NOT NULL,
   `title_three` varchar(255) NOT NULL,
-  `title_three_from` year(4) NOT NULL,
-  `title_three_to` year(4) NOT NULL,
+  `title_three_from` date NOT NULL,
+  `title_three_to` date NOT NULL,
   `title_three_venue` varchar(255) NOT NULL,
   `title_four` varchar(255) NOT NULL,
-  `title_four_from` year(4) NOT NULL,
-  `title_four_to` year(4) NOT NULL,
+  `title_four_from` date NOT NULL,
+  `title_four_to` date NOT NULL,
   `title_four_venue` varchar(255) NOT NULL,
   `title_five` varchar(255) NOT NULL,
-  `title_five_from` year(4) NOT NULL,
-  `title_five_to` year(4) NOT NULL,
+  `title_five_from` date NOT NULL,
+  `title_five_to` date NOT NULL,
   `title_five_venue` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lectures_and_seminars_attended`
+--
+
+INSERT INTO `lectures_and_seminars_attended` (`id`, `user_id`, `title_one`, `title_one_from`, `title_one_to`, `title_one_venue`, `title_two`, `title_two_from`, `title_two_to`, `title_two_venue`, `title_three`, `title_three_from`, `title_three_to`, `title_three_venue`, `title_four`, `title_four_from`, `title_four_to`, `title_four_venue`, `title_five`, `title_five_from`, `title_five_to`, `title_five_venue`) VALUES
+(31, 9, 'das', '2024-03-26', '2024-03-26', '2024-03-26', 'asd', '2024-03-26', '2024-03-26', '2024-03-26', 'da', '2024-03-26', '2024-03-26', '2024-03-26', '', '0000-00-00', '0000-00-00', '', '', '0000-00-00', '0000-00-00', '');
 
 -- --------------------------------------------------------
 
@@ -271,17 +331,19 @@ CREATE TABLE `user_resumes` (
   `philhealth_number` int(12) NOT NULL,
   `tin_number` int(12) NOT NULL,
   `contact_number` int(12) NOT NULL,
-  `educational_attainment_id` int(12) NOT NULL,
-  `empoloyment_background_id` int(12) NOT NULL,
-  `lectures_and_seminars_attended_id` int(12) NOT NULL,
-  `character_references_id` int(12) NOT NULL,
   `reference` varchar(255) NOT NULL,
   `additional_info_q1` varchar(255) NOT NULL,
-  `additional_info_q1v2` varchar(255) NOT NULL,
   `additional_info_q2` varchar(255) NOT NULL,
-  `declaration` tinyint(1) NOT NULL,
+  `declaration` varchar(255) NOT NULL,
   `authorization` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_resumes`
+--
+
+INSERT INTO `user_resumes` (`id`, `user_id`, `picture`, `email`, `last_name`, `first_name`, `middle_name`, `present_address`, `permanent_address`, `birthdate`, `gender`, `height`, `weight`, `nationality`, `religion`, `civil_status`, `sss_number`, `pagibig_number`, `philhealth_number`, `tin_number`, `contact_number`, `reference`, `additional_info_q1`, `additional_info_q2`, `declaration`, `authorization`) VALUES
+(5, 9, '6602359b0104b_default-profile-picture1.jpg', 'eq@gmail.com', 'ds', 'wq', 'eqw', 'asdqwe', 'sad', '2024-03-26', 'Male', 32, 32, 'hui', 'iu', 'Single', 765, 43, 53, 312, 231, '', 'das', 'dsa', 'agreed', '');
 
 --
 -- Indexes for dumped tables
@@ -343,19 +405,19 @@ ALTER TABLE `user_resumes`
 -- AUTO_INCREMENT for table `character_references`
 --
 ALTER TABLE `character_references`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `educational_attainment`
 --
 ALTER TABLE `educational_attainment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `employment_background`
 --
 ALTER TABLE `employment_background`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -373,7 +435,7 @@ ALTER TABLE `job_applicants`
 -- AUTO_INCREMENT for table `lectures_and_seminars_attended`
 --
 ALTER TABLE `lectures_and_seminars_attended`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -385,7 +447,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user_resumes`
 --
 ALTER TABLE `user_resumes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
